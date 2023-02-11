47a8a4ac61e5a5a8599737cae79b9b8c350b293b
Xp <1014647664@qq.com>
2023-02-11 10:37:26 +0800

    实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定

--- day_15/demo_01/main.c
+++ day_15/demo_01/main.c
@@ -6,9 +6,11 @@ int main() {
     scanf("%d",&input);
     for (int i = 1; i <= input; i++) {
         for (int j = 1; j <= i; ++j) {
-            printf("%d*%d=%-4d", i, j, i * j);
+            printf("%-2d*%d=%-4d", i, j, i * j);
         }
         printf("\n");
     }
+
     return 0;
 }
+
