c3081f5b4958877250a36442e93708608baa73c8
Xp <1014647664@qq.com>
2023-02-11 10:39:45 +0800

    实现一个函数来交换两个整数的内容。

--- day_15/demo_02/main.c
+++ day_15/demo_02/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void Swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    //实现一个函数来交换两个整数的内容。
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    Swap(&a, &b);
+    printf("%d %d\n", a, b);
     return 0;
 }
