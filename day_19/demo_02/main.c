7ec38544cc877f67fce31e0754fd76336a505d27
Xp <1014647664@qq.com>
2023-02-17 13:01:04 +0800

    模拟实现strcpy

--- day_19/demo_02/main.c
+++ day_19/demo_02/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <assert.h>
 
-//const
+char *my_strcpy(char *dest, const char *src) {
+    assert(dest);//断言，防止传过来空指针
+    assert(src);
+    char *ret = dest;//保留目的字符串的原始地址
+    while (*dest++ = *src++) { ;
+    }
+    return ret;
+}
+
+//模拟实现strcpy
 int main() {
-    printf("Hello, World!\n");
+    char arr1[] = "xxxxxxxxxxxxx";
+    char arr2[] = "hello";
+    printf("%s", my_strcpy(arr1, arr2));
     return 0;
 }
